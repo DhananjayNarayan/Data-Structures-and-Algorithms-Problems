99be3f5a5b3dfe6ae5832493f58090e3a46f2a3c
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-02-21 19:11:00 +0530

    Update FilesMaps.java

--- Files and Maps Demo/FilesMaps.java
+++ Files and Maps Demo/FilesMaps.java
@@ -3,22 +3,28 @@ import java.io.*;
 public class FilesMaps {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Map<String, Integer> mp = new HashMap<>();
-		mp=ReadMap("India.txt");
-		mp.forEach((key, value) -> System.out.println(key + " : " + value));
-		//System.out.println(Arrays.asList(map));
+		
+		showMap("India.txt");
+		
 		
 	}
-
+ 
+	public static void showMap(String filename) {
+		Map<String, Integer> mp = new HashMap<>();
+		mp=ReadMap(filename);
+		mp.forEach((key, value) -> System.out.println(key + " : " + value));
+		//System.out.println(Arrays.asList(mp)+"\n");
+	}
+	
+	
 	public static Map<String, Integer> ReadMap(String filename) 
 	{
 		Scanner sc = null  ;
 		Map<String, Integer> map = new HashMap<>();
 		try {
-			 sc= new Scanner(new File("India.txt"));
+			 sc= new Scanner(new File(filename));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Invalid filename or the file does not exist");
 			e.printStackTrace();
 		}
 		
@@ -26,7 +32,7 @@ public class FilesMaps {
 			String a=sc.next();
 			int b=sc.nextInt();
 			map.put(a, b);
-			//System.out.printf("%s %d\n",a,b);
+		//System.out.printf("%s %d\n",a,b);
 		}
 		return map;
 	}
